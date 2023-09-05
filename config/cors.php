1e161cbf59b55c67c1b93c682c5c9686be84c8e1
Ricardo Chinchay <ricardochinchay@Ricardos-MacBook-Pro.local>
2023-09-05 14:33:54 -0300

    instalacion de laravel/breeze y configuracion tipo api

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
@@ -31,6 +31,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
