07f6f351cffea4bae822c970b92889d8b0cca912
Ricardo Chinchay <ricardochinchay@Ricardos-MacBook-Pro.local>
2023-09-05 11:59:30 -0300

    formateo de codigo inicial

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,10 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+/**
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @coversNothing
+ */
 class ExampleTest extends TestCase
 {
     /**
